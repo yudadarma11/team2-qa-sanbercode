8c9c4f45fe8d4e9501c6e4598a319ab9d44d1ec6
Yuda Darma <76606722+yudadarma11@users.noreply.github.com>
2022-12-30 21:57:24 +0700

    Update add_job_titles.py

--- admin/job_titles/add_job_titles.py
+++ admin/job_titles/add_job_titles.py
@@ -20,23 +20,23 @@ class TestLogin(unittest.TestCase):
         time.sleep(1)
         browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123") # isi password by XPATH, #email tidak boleh sama
         time.sleep(1)
-        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol login 
         time.sleep(3)
 
         #Open page Admin
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span").click() # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a/span").click() # klik menu Admin
         time.sleep(3)
 
-        #Open Tab Nationalities
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/span").click() # klik tombol login by XPATH
+        #Open Job Titles
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/span").click() # klik sub menu Job
         time.sleep(2)
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[1]/a").click() # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[1]/a").click() # klik Job Titles
         time.sleep(3)
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/div[1]/div/button").click() # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/div[1]/div/button").click() # klik tombol +Add
         time.sleep(3)
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("COBA") # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input").send_keys("COBA") # Isi kolom Job Title
         time.sleep(3)
-        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click() # klik tombol login by XPATH
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[5]/button[2]").click() # klik tombol save
         time.sleep(8)
 
     def tearDown(self): 
